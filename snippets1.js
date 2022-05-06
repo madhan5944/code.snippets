bb7e15bb48f484be0b79a595e4854dae95363129
madhan5944 <99804685+madhan5944@users.noreply.github.com>
2022-05-06 12:31:31 +0530

    Add files via upload

---
+++ snippets1.js
@@ -0,0 +1,7 @@
+let a="5";
+if(a===5){
+    console.log("This is a tricky question");
+}
+else{
+    console.log("ÿou figured it out" );
+}
---
+++ snippets2.js
@@ -0,0 +1,8 @@
+let str="management";
+if(1===1){
+    str="pancake";
+}
+if(str[2]==='n'){
+    str="alphines";
+}
+console.log(str);
