58aecc88bb3dcdb1689fbefd2c8a9bb031e51c01
madhan5944 <99804685+madhan5944@users.noreply.github.com>
2022-05-06 13:10:27 +0530

    Add files via upload

---
+++ snippets3.js
@@ -0,0 +1,7 @@
+const arr=[3,4,5,6,7,8]
+if((arr[3]+4)===10)
+{
+arr.push(10);
+arr[3]=0;
+}
+console.log(arr);
---
+++ snippets4.js
@@ -0,0 +1,9 @@
+const obj={
+    name:"arun",
+    age:"17",
+    school:"skv"
+
+}
+console.log(obj.name);
+obj.std=11;
+console.log(obj);
